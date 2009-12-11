629d2f6613998a746778115c5d23cf6b0357041b
David Young-Chan KaY <david.kay@icihere.com>
2009-12-11 10:05:18 -0500

    Added resources on mode-X

--- davidk.asm
+++ davidk.asm
@@ -8,14 +8,12 @@ include davidk.inc
 ;DT Reserves space in one Ten (10 byte) units.  
 DGROUP  GROUP   _DATA, STACK
 STACK   SEGMENT PARA STACK 'STACK'
-        DB      256 DUP (?)
+        DB      256 DUP (?) ;DUP for duplicate, ie 'fill the space with the following'
 STACK   ENDS
 _DATA   SEGMENT PARA PUBLIC 'DATA'
 screen  DD      0a0000000h
 deltx   DW      0000h
 delty   DW      0000h
-;oldtime DB		?
-;deltTime DB		?
 ;screen  DD      0a0007D00h ; halfway down screen
 ;screen  DD      0a0007D6Ah ; line in middle
 ;buffer	 DW					; dedicate a WORD for our buffer?
