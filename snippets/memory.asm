1baaf30f5a1b939665d51b47476d0b07f8e1377e
David Young-Chan KaY <david.kay@icihere.com>
2009-12-13 09:50:04 -0500

    Added call to resize_buffer, but it doesn't seem to be working just yet.

--- snippets/memory.asm
+++ snippets/memory.asm
@@ -13,50 +13,57 @@ _DATA   SEGMENT PARA PUBLIC 'DATA'
 emsg	DB	"Memory error", 13, 10, "$"
 amsg	DB	"Memory allocation success", 13, 10, "$"
 bmsg	DB	"Memory release success", 13, 10, "$"
-;screen  DD      0a0000000h
-;deltx   DW      0000h
-;delty   DW      0000h
-;screen  DD      0a0007D00h ; halfway down screen
-;screen  DD      0a0007D6Ah ; line in middle
+cmsg	DB	"Memory resize success", 13, 10, "$"
 _DATA   ENDS
 _TEXT   SEGMENT PARA PUBLIC 'CODE'
         ASSUME  cs:_TEXT, ds:DGROUP, ss:DGROUP
 start:
         mov     ax, DGROUP
         mov     ds, ax
+		call	resize_buffer
 		call	allocate_buffer
 		call	deallocate_buffer
 		jmp		done
 allocate_buffer: ;uses DOS int 21, 48h to allocate memory
 		push	bx
 		;mov		bx, 4000	;BX: number of paragraphs to allocate
-		mov		bx, 1	;BX: number of paragraphs to allocate
+		mov		bx, 7			;BX: number of paragraphs to allocate
 		mov		ah, 48h
-        int     21h			;DOS allocate memory interrupt
-		jc		error ;check for error
+        int     21h				;DOS allocate memory interrupt
+		jc		error			;check for error
 		call	success1
 		pop		bx
 		ret
 deallocate_buffer:
 		push	bx
-		mov		es,	ax		;segment of block to free
+		mov		es,	ax			;segment of block to free
 		mov		ah, 49h
-        int     21h			;DOS allocate memory interrupt
-		jc		error ;check for error
+        int     21h				;DOS allocate memory interrupt
+		jc		error			;check for error
 		call	success2
 		pop		bx
 		ret
-success1:
-		push	ax
-		mov	ah, 09h
-		mov	dx, OFFSET amsg
-		int	21h						;write to terminal
-		pop		ax
+resize_buffer:
+		push	bx
+		mov		es,	ax			;segment of block to free
+		mov		bx, 2000		;new size in paragraphs
+		mov		ah, 4Ah
+        int     21h				;DOS allocate memory interrupt
+		jc		error			;check for error
+		call	success3
+		pop		bx
 		ret
+success1:
+		mov	dx, OFFSET amsg
+		jmp print_success
 success2:
+		mov	dx, OFFSET bmsg
+		jmp print_success
+success3:
+		mov	dx, OFFSET cmsg
+print_success:
 		push	ax
 		mov	ah, 09h
-		mov	dx, OFFSET bmsg
 		int	21h						;write to terminal
 		pop		ax
 		ret
